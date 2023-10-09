06257f9f79885898fae1d1f12515d2f2745ef2de
Aimer Neige <aimer.neige.soft@gmail.com>
2023-10-09 19:33:48 +0800

    assets

--- install.sh
+++ install.sh
@@ -252,9 +252,9 @@ sudo DEBIAN_FRONTEND=noninteractive apt upgrade -y
 
 # Fix CJK
 sudo wget https://gitlab.aiursoft.cn/anduin/anduinos/-/raw/master/Config/fonts.conf -O /etc/fonts/local.conf
-wget https://aimerneige.aiur.site/fonts/ubuntu-setup-fonts.zip
-sudo unzip ubuntu-setup-fonts.zip -d /usr/share/fonts/
-rm -f ubuntu-setup-fonts.zip
+wget https://gitlab.aiursoft.cn/anduin/anduinos/-/raw/master/Assets/fonts.zip
+sudo unzip fonts.zip -d /usr/share/fonts/
+rm -f fonts.zip
 sudo fc-cache -fv
 
 # Theme
