90982efc4360b55e7ea3234b41c5452ed3d981e3
AnduinXue <anduin@aiursoft.com>
2024-03-11 19:13:05 +0000

    Google earth setup key.

--- install.sh
+++ install.sh
@@ -82,7 +82,8 @@ echo 'deb [arch=amd64 signed-by=/etc/apt/keyrings/google-chrome.gpg] http://dl.g
 
 # Google Earth Pro
 print_ok "Setting google earth pro..."
-sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/earth/deb/ stable main" > /etc/apt/sources.list.d/google.list'
+wget -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/earth.gpg > /dev/null 2>&1
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/earth/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 
 # Code
 print_ok "Setting VSCode..."
