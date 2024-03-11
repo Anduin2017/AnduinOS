a7453e084d16c5149a3b2acf5c2c1cc657e032ad
AnduinXue <anduin@aiursoft.com>
2024-03-11 19:06:40 +0000

    Call sudo add chrome key.

--- install.sh
+++ install.sh
@@ -77,7 +77,7 @@ echo \
 # Google Chrome Source
 print_ok "Setting google chrome..."
 wget https://dl-ssl.google.com/linux/linux_signing_key.pub -O /tmp/google.pub
-gpg --no-default-keyring --keyring /etc/apt/keyrings/google-chrome.gpg --import /tmp/google.pub
+sudo gpg --no-default-keyring --keyring /etc/apt/keyrings/google-chrome.gpg --import /tmp/google.pub
 echo 'deb [arch=amd64 signed-by=/etc/apt/keyrings/google-chrome.gpg] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
 
 # Google Earth Pro
