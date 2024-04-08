7bcfe916feec7a423d1a5ae1e7383fde3eb41c02
Anduin Xue <anduin@aiursoft.com>
2024-04-08 16:51:02 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -104,6 +104,7 @@ print_ok "Setting docker..."
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
+judge "Trust docker gpg"
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
