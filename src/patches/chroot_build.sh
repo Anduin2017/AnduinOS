4cc5f18bded511008edbf616e3ba33d685ead529
Anduin Xue <anduin@aiursoft.com>
2024-08-01 14:34:14 +0000

    Patch Shotwell and rhythmbox localization

--- src/patches/chroot_build.sh
+++ src/patches/chroot_build.sh
@@ -273,6 +273,14 @@ EOF
     sed -i '/^Name=/a Name[zh_TW]=圖庫' /usr/share/applications/shotwell.desktop
     sed -i '/^X-GNOME-FullName=/a X-GNOME-FullName[zh_CN]=图库' /usr/share/applications/shotwell.desktop
     sed -i '/^X-GNOME-FullName=/a X-GNOME-FullName[zh_TW]=圖庫' /usr/share/applications/shotwell.desktop
+    judge "Patch Shotwell localization"
+
+    print_ok "Patch rhythmbox localization..."
+    sed -i '/^Name=/a Name[zh_CN]=音乐' /usr/share/applications/rhythmbox.desktop
+    sed -i '/^Name=/a Name[zh_TW]=音樂' /usr/share/applications/rhythmbox.desktop
+    sed -i '/^X-GNOME-FullName=/a X-GNOME-FullName[zh_CN]=音乐' /usr/share/applications/rhythmbox.desktop
+    sed -i '/^X-GNOME-FullName=/a X-GNOME-FullName[zh_TW]=音樂' /usr/share/applications/rhythmbox.desktop
+    judge "Patch rhythmbox localization"
 
     # Redirect /usr/local/bin/gnome-terminal -> /usr/bin/kgx
     print_ok "Redirect /usr/local/bin/gnome-terminal -> /usr/bin/kgx"
