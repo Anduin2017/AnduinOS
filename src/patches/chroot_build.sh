65adcac46133360a34332ff36efe2316cd4cd102
Anduin Xue <anduin@aiursoft.com>
2024-08-03 13:04:46 +0000

    Update os-release and casper.conf files for AnduinOS branding

--- src/patches/chroot_build.sh
+++ src/patches/chroot_build.sh
@@ -492,6 +492,25 @@ PRIVACY_POLICY_URL="https://www.ubuntu.com/legal/terms-and-policies/privacy-poli
 UBUNTU_CODENAME=$TARGET_UBUNTU_VERSION
 EOF
     judge "Update os-release"
+
+    print_ok "Updating /etc/casper.conf"
+    cat << EOF > /etc/casper.conf
+# This file should go in /etc/casper.conf
+# Supported variables are:
+# USERNAME, USERFULLNAME, HOST, BUILD_SYSTEM, FLAVOUR
+
+export USERNAME="live"
+export USERFULLNAME="AnduinOS Live session user"
+export HOST="anduinos"
+export BUILD_SYSTEM="Ubuntu"
+
+# USERNAME and HOSTNAME as specified above won't be honoured and will be set to
+# flavour string acquired at boot time, unless you set FLAVOUR to any
+# non-empty string.
+
+export FLAVOUR="AnduinOS"
+EOF
+    judge "Update /etc/casper.conf"
 }
 
 function clean_up() {
