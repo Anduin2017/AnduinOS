1e88a17a6e5fca5e2f092dbe545b4e3a18f9220c
Anduin Xue <anduin@aiursoft.com>
2024-08-03 17:33:40 +0000

    chore: Truncate machine id and remove diversion in chroot_build.sh

--- src/patches/chroot_build.sh
+++ src/patches/chroot_build.sh
@@ -574,6 +574,7 @@ function finish_up() {
     print_ok "Truncating machine id..."
     truncate -s 0 /etc/machine-id
     truncate -s 0 /var/lib/dbus/machine-id
+    judge "Truncate machine id"
 
     # remove diversion (why??)
     print_ok "Removing diversion..."
