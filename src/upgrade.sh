579c841fa9997192012439e91df764a1cc0a8ee2
Anduin Xue <anduin@aiursoft.com>
2024-08-04 15:07:26 +0000

    chore: Upgrade AnduinOS to version 0.1.2-beta

--- src/upgrade.sh
+++ src/upgrade.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # This script can only run on AnduinOS. It will detect current OS distro first. Stop running on other distros.
 # This script will compare current version based on: /etc/lsb-release and latest version from the repository
 # This script will see if there is a new version available and if so, it will upgrade the system
@@ -6,6 +7,177 @@
 # If there is no new version available, this script will do nothing, only output a message
 # Finally this script will update the /etc/lsb-release, /etc/os-release and /etc/issue files with the latest version
 
-echo "This script will upgrade the AnduinOS to the latest version."
+#==========================
+# Set up the environment
+#==========================
+set -e                  # exit on error
+set -o pipefail         # exit on pipeline error
+set -u                  # treat unset variable as error
+export DEBIAN_FRONTEND=noninteractive
+export LATEST_VERSION="0.1.2-beta"
+export CURRENT_VERSION=$(cat /etc/lsb-release | grep DISTRIB_RELEASE | cut -d "=" -f 2)
 
-echo "No new version available. Your system is up to date."
+#==========================
+# Color
+#==========================
+Green="\033[32m"
+Red="\033[31m"
+Yellow="\033[33m"
+Blue="\033[36m"
+Font="\033[0m"
+GreenBG="\033[42;37m"
+RedBG="\033[41;37m"
+OK="${Green}[  OK  ]${Font}"
+ERROR="${Red}[FAILED]${Font}"
+WARNING="${Yellow}[ WARN ]${Font}"
+
+#==========================
+# Print Colorful Text
+#==========================
+function print_ok() {
+  echo -e "${OK} ${Blue} $1 ${Font}"
+}
+
+function print_error() {
+  echo -e "${ERROR} ${Red} $1 ${Font}"
+}
+
+function print_warn() {
+  echo -e "${WARNING} ${Yellow} $1 ${Font}"
+}
+
+#==========================
+# Judge function
+#==========================
+function judge() {
+  if [[ 0 -eq $? ]]; then
+    print_ok "$1 succeeded"
+    sleep 1
+  else
+    print_error "$1 failed"
+    exit 1
+  fi
+}
+
+#==========================
+# Are you sure function
+#==========================
+function areYouSure() {
+  print_error "This script found some issue and failed to run. Continue may cause system unstable."
+  print_error "Are you sure to continue the installation? Enter [y/N] to continue"
+  read -r install
+  case $install in
+  [yY][eE][sS] | [yY])
+    print_ok "Continue the installation"
+    ;;
+  *)
+    print_error "Installation terminated"
+    exit 1
+    ;;
+  esac
+}
+
+function ensureCurrentOsAnduinOs() {
+    # Ensure the current OS is AnduinOS
+    if ! grep -q "DISTRIB_ID=AnduinOS" /etc/lsb-release; then
+        print_error "This script can only be run on AnduinOS."
+        exit 1
+    fi
+}
+
+function upgrade_010_to_011() {
+    # Add your upgrade steps from 0.1.0 to 0.1.1 here
+    print_ok "Upgrading from 0.1.0 to 0.1.1"
+
+    print_ok "Adding do_anduinos_upgrade command"
+    sudo bash -c 'cat << EOF > /usr/local/bin/do_anduinos_upgrade
+#!/bin/bash
+echo "Upgrading AnduinOS..."
+curl -sSL https://gitlab.aiursoft.cn/anduin/anduinos/-/raw/master/src/upgrade.sh | bash
+EOF
+'
+    sudo chmod +x /usr/local/bin/do_anduinos_upgrade
+    judge "Add do_anduinos_upgrade command"
+
+    print_ok "Adding new app called AnduinOS Software..."
+    sudo bash -c 'cat << EOF > /usr/share/applications/anduinos-software.desktop
+[Desktop Entry]
+Name=Apps Store
+GenericName=Apps Store
+Name[zh_CN]=应用商店
+Name[zh_TW]=應用商店
+Comment=Browse AnduinOS software collection and install our verified applications
+Comment[zh_CN]=浏览 AnduinOS 的软件商店并安装我们验证过的应用
+Comment[zh_TW]=瀏覽 AnduinOS 的軟體商店並安裝我們驗證過的應用
+Categories=System;
+Exec=xdg-open https://docs.anduinos.com/Applications/Introduction.html
+Terminal=false
+Type=Application
+Icon=system-software-install
+StartupNotify=true
+EOF
+'
+    judge "Add new app called AnduinOS Software"
+
+}
+
+function upgrade_011_to_012() {
+    # Add your upgrade steps from 0.1.1 to 0.1.2 here
+    print_ok "Upgrading from 0.1.1 to 0.1.2"
+
+    print_ok "Installing ubuntu-drivers-common"
+    sudo apt install ubuntu-drivers-common -y
+    judge "Install ubuntu-drivers-common"
+
+    print_ok "Installing apport"
+    sudo apt autoremove apport -y
+    judge "Install apport"
+
+    print_ok "Upgrade to 0.1.2-beta succeeded"
+}
+
+function applyLsbRelease() {
+    # Update /etc/lsb-release
+    sudo sed -i "s/DISTRIB_RELEASE=.*/DISTRIB_RELEASE=${LATEST_VERSION}/" /etc/lsb-release
+    sudo sed -i "s/DISTRIB_DESCRIPTION=.*/DISTRIB_DESCRIPTION=\"AnduinOS ${LATEST_VERSION}\"/" /etc/lsb-release
+    
+    # Update /etc/os-release
+    sudo sed -i "s/VERSION_ID=.*/VERSION_ID=\"${LATEST_VERSION}\"/" /etc/os-release
+    sudo sed -i "s/VERSION=.*/VERSION=\"${LATEST_VERSION} (jammy)\"/" /etc/os-release
+    sudo sed -i "s/PRETTY_NAME=.*/PRETTY_NAME=\"AnduinOS ${LATEST_VERSION}\"/" /etc/os-release
+
+    # Update /etc/issue
+    echo "AnduinOS ${LATEST_VERSION} \n \l" | sudo tee /etc/issue > /dev/null
+}
+
+function main() {
+    # Ensure the current OS is AnduinOS
+    ensureCurrentOsAnduinOs
+
+    # Compare current version with latest version
+    if [ "$CURRENT_VERSION" == "$LATEST_VERSION" ]; then
+        print_ok "Your system is already up to date. No updates available."
+        exit 0
+    fi
+
+    # Run necessary upgrades based on current version
+    case "$CURRENT_VERSION" in
+        "0.1.0-beta")
+            upgrade_010_to_011
+            upgrade_011_to_012
+            ;;
+        "0.1.1-beta")
+            upgrade_011_to_012
+            ;;
+        *)
+            print_error "Unknown current version. Exiting."
+            exit 1
+            ;;
+    esac
+
+    # Apply updates to lsb-release, os-release, and issue files
+    applyLsbRelease
+    print_ok "System upgraded successfully to version ${LATEST_VERSION}"
+}
+
+main
