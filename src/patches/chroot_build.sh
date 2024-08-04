97cf6947459e1ce99f34a5fa35bb8234dbb02782
Anduin Xue <anduin@aiursoft.com>
2024-08-04 17:25:02 +0000

    chore: Update chroot_build.sh to install curl for systemd configuration

--- src/patches/chroot_build.sh
+++ src/patches/chroot_build.sh
@@ -118,9 +118,8 @@ EOF
 
     # we need to install systemd first, to configure machine id
     print_ok "Installing systemd..."
-    waitNetwork
     apt update
-    apt install -y libterm-readline-gnu-perl systemd-sysv
+    apt install -y libterm-readline-gnu-perl systemd-sysv curl
     judge "Install systemd"
 
     #configure machine id
