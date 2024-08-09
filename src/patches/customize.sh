c269b67fd4543cac0f5b8f39ec4291f7c5d67b5c
Anduin Xue <anduin@aiursoft.com>
2024-08-09 06:36:56 +0000

    chore: Update TARGET_UBUNTU_MIRROR URL in customize.sh

--- src/patches/customize.sh
+++ src/patches/customize.sh
@@ -2,7 +2,7 @@
 
 export TARGET_UBUNTU_VERSION="jammy"
 export BUILD_UBUNTU_MIRROR="http://mirror.aiursoft.cn/ubuntu/"
-export TARGET_UBUNTU_MIRROR="http://mirror.anduinos.com/ubuntu/"
+export TARGET_UBUNTU_MIRROR="http://mirrors.anduinos.com/ubuntu/"
 export TARGET_NAME="anduinos"
 export TARGET_BUSINESS_NAME="AnduinOS"
 export TARGET_BUILD_VERSION="0.1.3-beta"
