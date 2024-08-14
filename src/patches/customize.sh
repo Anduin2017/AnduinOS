d504c632d7e17f5c051e73af0944ac6252b23ae9
Anduin Xue <anduin@aiursoft.com>
2024-08-14 08:58:00 +0000

    Hide network stats icon.

--- src/patches/customize.sh
+++ src/patches/customize.sh
@@ -5,7 +5,7 @@ export BUILD_UBUNTU_MIRROR="http://mirror.aiursoft.cn/ubuntu/"
 export TARGET_UBUNTU_MIRROR="http://mirrors.anduinos.com/ubuntu/"
 export TARGET_NAME="anduinos"
 export TARGET_BUSINESS_NAME="AnduinOS"
-export TARGET_BUILD_VERSION="0.1.4-beta"
+export TARGET_BUILD_VERSION="0.1.5-beta"
 export TARGET_PACKAGE_REMOVE="
     ubiquity \
     casper \
