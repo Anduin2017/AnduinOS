1d7c63c37dac06692a1b045beb88cf84604c5bbb
Anduin Xue <anduin@aiursoft.com>
2024-08-16 10:32:08 +0000

    chore: Update chroot_build.sh to use new Ubuntu mirror
    This commit updates the `chroot_build.sh` script to use a new Ubuntu mirror, changing the `BUILD_UBUNTU_MIRROR` environment variable to "http://mirror.aiursoft.cn/ubuntu/". This change ensures faster and more reliable package downloads during the build process.

--- src/patches/customize.sh
+++ src/patches/customize.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export TARGET_UBUNTU_VERSION="jammy"
-export BUILD_UBUNTU_MIRROR="http://tw.archive.ubuntu.com/ubuntu/"
+export BUILD_UBUNTU_MIRROR="http://mirror.aiursoft.cn/ubuntu/"
 export TARGET_UBUNTU_MIRROR="http://mirrors.anduinos.com/ubuntu/"
 export TARGET_NAME="anduinos"
 export TARGET_BUSINESS_NAME="AnduinOS"
