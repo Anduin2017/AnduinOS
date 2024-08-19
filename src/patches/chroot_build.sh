7de344e90d5c9ec7c06337e98eb092a937293d51
Anduin Xue <anduin@aiursoft.com>
2024-08-19 06:26:29 +0000

    chore: Comment out unused repository source in chroot_build.sh

--- src/patches/chroot_build.sh
+++ src/patches/chroot_build.sh
@@ -260,7 +260,7 @@ EOF
     waitNetwork
     apt install -y software-properties-common
     add-apt-repository -y ppa:mozillateam/ppa -n
-    echo "deb https://mirror-ppa.aiursoft.cn/mozillateam/ppa/ubuntu/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/mozillateam-ubuntu-ppa-$(lsb_release -sc).list
+    #echo "deb https://mirror-ppa.aiursoft.cn/mozillateam/ppa/ubuntu/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/mozillateam-ubuntu-ppa-$(lsb_release -sc).list
     cat << EOF > /etc/apt/preferences.d/mozilla-firefox
 Package: *
 Pin: release o=LP-PPA-mozillateam
