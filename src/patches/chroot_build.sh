b7fda44b8de0c9bac5733461ce155b4f1d09875e
Anduin Xue <anduin@aiursoft.com>
2024-08-20 11:08:55 +0000

    chore: Patch baobab and rhythmbox localizations

--- src/patches/chroot_build.sh
+++ src/patches/chroot_build.sh
@@ -336,12 +336,19 @@ EOF
     sed -i '/^X-GNOME-FullName=/a X-GNOME-FullName[zh_TW]=圖庫' /usr/share/applications/shotwell.desktop
     judge "Patch Shotwell localization"
 
-    print_ok "Patch rhythmbox localization..."
+    print_ok "Patching rhythmbox localization..."
     sed -i '/^Name=/a Name[zh_CN]=音乐' /usr/share/applications/rhythmbox.desktop
     sed -i '/^Name=/a Name[zh_TW]=音樂' /usr/share/applications/rhythmbox.desktop
     sed -i '/^X-GNOME-FullName=/a X-GNOME-FullName[zh_CN]=音乐' /usr/share/applications/rhythmbox.desktop
     sed -i '/^X-GNOME-FullName=/a X-GNOME-FullName[zh_TW]=音樂' /usr/share/applications/rhythmbox.desktop
     judge "Patch rhythmbox localization"
+
+    print_ok "Patching baobab localization..."
+    sed -i '/^Name=/a Name[zh_CN]=磁盘分析' /usr/share/applications/org.gnome.baobab.desktop
+    sed -i '/^Name=/a Name[zh_TW]=磁碟分析' /usr/share/applications/org.gnome.baobab.desktop
+    sed -i '/^X-GNOME-FullName=/a X-GNOME-FullName[zh_CN]=磁盘分析' /usr/share/applications/org.gnome.baobab.desktop
+    sed -i '/^X-GNOME-FullName=/a X-GNOME-FullName[zh_TW]=磁碟分析' /usr/share/applications/org.gnome.baobab.desktop
+    judge "Patch baobab localization"
 
     print_ok "Adding new app called AnduinOS Software..."
     cat << EOF > /usr/share/applications/anduinos-software.desktop
