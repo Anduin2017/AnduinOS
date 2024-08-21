11f3e4b5b89cabc0cd9276c8159381b3a2ca19e4
Anduin Xue <anduin@aiursoft.com>
2024-08-21 16:34:42 +0000

    chore: Set firefox homepage to https://www.anduinos.com/

--- src/mods/13-firefox-patch-source-mod/install.sh
+++ src/mods/13-firefox-patch-source-mod/install.sh
@@ -14,3 +14,17 @@ judge "Add Mozilla Firefox PPA"
 print_ok "Updating package list to refresh firefox package cache"
 apt update
 judge "Update package list"
+
+print_ok "Setting firefox homepage to https://www.anduinos.com/"
+sudo mkdir -p /usr/lib/firefox/defaults/pref
+cat << EOF > /usr/lib/firefox/defaults/pref/autoconfig.js
+pref("general.config.filename", "mozilla.cfg");
+pref("general.config.obscure_value", 0);
+EOF
+cat << EOF > /usr/lib/firefox/mozilla.cfg
+// Mozilla Firefox Configuration
+// Homepage
+lockPref("browser.startup.homepage", "https://www.anduinos.com/");
+EOF
+chown -R root:root /usr/lib/firefox
+judge "Set firefox homepage"
