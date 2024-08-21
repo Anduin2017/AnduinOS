bbc8ef900d22dff4a6c3db13a02b5776a5fe1a50
Anduin Xue <anduin@aiursoft.com>
2024-08-21 08:36:58 +0000

    chore: Add purge, wallpaper, icon, and GTK themes installation scripts

---
+++ src/mods/24-fluent-icon-theme/install.sh
@@ -0,0 +1,21 @@
+print_ok "Downloading Fluent icon theme"
+git clone https://git.aiursoft.cn/PublicVault/Fluent-icon-theme ./themes/Fluent-icon-theme
+judge "Download Fluent icon theme"
+
+print_ok "Installing Fluent icon theme"
+(
+    print_ok "Installing Fluent icon theme" && \
+    cd ./themes/Fluent-icon-theme/ && \
+    ./install.sh standard
+)
+judge "Install Fluent icon theme"
+
+#==============================================
+
+print_ok "Installing Fluent cursor theme"
+(
+    print_ok "Installing Fluent cursor theme" && \
+    cd ./themes/Fluent-icon-theme/cursors/ && \
+    ./install.sh
+)
+judge "Install Fluent cursor theme"
