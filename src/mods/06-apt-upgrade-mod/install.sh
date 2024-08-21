3f2de07d27e2f0f4f8c13d6fa95144342435225a
Anduin Xue <anduin@aiursoft.com>
2024-08-21 09:17:09 +0000

    chore: Update apt upgrade message to clarify the reason for updating packages

--- src/mods/06-apt-upgrade-mod/install.sh
+++ src/mods/06-apt-upgrade-mod/install.sh
@@ -1,4 +1,4 @@
-print_ok "Updating packages..."
+print_ok "Updating packages...(This is because debootstrap may not have the latest package list)"
 waitNetwork
 apt -y upgrade
 judge "Upgrade packages"
