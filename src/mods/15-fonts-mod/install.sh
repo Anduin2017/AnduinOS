fe202f9271935ef30a396489bffa1806d83cfc5d
Anduin Xue <anduin@aiursoft.com>
2024-08-21 08:28:11 +0000

    chore: Update installation scripts and patch Ubiquity installer

--- src/mods/15-fonts-mod/install.sh
+++ src/mods/15-fonts-mod/install.sh
@@ -2,3 +2,7 @@ print_ok "Patching fonts..."
 cp ./local.conf /etc/fonts/
 unzip ./fonts.zip -d /usr/share/fonts/
 judge "Patch fonts"
+
+print_ok "Updating font cache"
+fc-cache -f -v
+judge "Update font cache"
---
+++ src/mods/17-appstore-app/install.sh
@@ -0,0 +1,17 @@
+print_ok "Adding new app called AnduinOS Software..."
+cat << EOF > /usr/share/applications/anduinos-software.desktop
+[Desktop Entry]
+Name=Apps Store
+GenericName=Apps Store
+Name[zh_CN]=应用商店
+Name[zh_TW]=應用商店
+Comment=Browse AnduinOS's software collection and install our verified applications
+Comment[zh_CN]=浏览 AnduinOS 的软件商店并安装我们验证过的应用
+Comment[zh_TW]=瀏覽 AnduinOS 的軟體商店並安裝我們驗證過的應用
+Categories=System;
+Exec=xdg-open https://docs.anduinos.com/Applications/Introduction.html
+Terminal=false
+Type=Application
+Icon=system-software-install
+StartupNotify=true
+EOF
