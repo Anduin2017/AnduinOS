b0ca66c80530f44688f9fd1db689e69a7a995774
Anduin Xue <anduin@aiursoft.com>
2024-08-24 07:17:21 +0000

    chore: Update language environment and localization for Chinese translations
    This commit updates the language environment in the args.sh script to support Chinese translations. It sets the language mode to "zh_CN" and the language pack code to "zh". Additionally, it configures the locales and resolvconf to use the specified language. The upgrade.sh script is also updated to patch the localization for Chinese translations, ensuring that the application name is properly translated for Chinese users. These changes improve the language support and localization for Chinese users.

--- src/mods/02-set-hostname-mod/install.sh
+++ src/mods/02-set-hostname-mod/install.sh
@@ -2,3 +2,13 @@ print_ok "Setting up hostname..."
 echo "$TARGET_NAME" > /etc/hostname
 hostname "$TARGET_NAME"
 judge "Set up hostname to $TARGET_NAME"
+
+print_ok "Configuring locales and resolvconf..."
+apt update
+apt install -y locales resolvconf
+judge "Install locales and resolvconf"
+
+print_ok "Configuring locales and resolvconf to $LANG..."
+locale-gen $LANG
+update-locale LANG=$LANG
+judge "Configure locales and resolvconf"
--- src/mods/03-systemd-mod/install.sh
+++ src/mods/03-systemd-mod/install.sh
@@ -4,10 +4,5 @@ print_ok "Installing systemd"
 # Don't wait for network, because curl is not available
 #waitNetwork
 apt update
-apt install -y libterm-readline-gnu-perl systemd-sysv curl locales resolvconf
+apt install -y libterm-readline-gnu-perl systemd-sysv curl
 judge "Install systemd"
-
-print_ok "Configuring locales and resolvconf to $LANG..."
-locale-gen $LANG
-update-locale LANG=$LANG
-judge "Configure locales and resolvconf"
