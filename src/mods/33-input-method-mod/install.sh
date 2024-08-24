b8ba8b9f35aa3758a08a301e0ce5b5d82441c788
Anduin Xue <anduin@aiursoft.com>
2024-08-24 09:56:27 +0000

    chore: Update input method installation script
    This commit updates the input method installation script to improve compatibility and ensure correct installation of ibus-rime. The script now uses the "-y" flag when installing ibus-rime to automatically confirm the installation. Additionally, it creates the necessary directory "/etc/skel/.config/ibus/rime" before syncing the assets from the anduinos-rime repository. The "ibus restart" and "ibus engine rime" commands have been removed as they are not necessary for the installation. These changes enhance the installation process and ensure proper setup of the input method.

--- src/mods/33-input-method-mod/install.sh
+++ src/mods/33-input-method-mod/install.sh
@@ -1,6 +1,6 @@
 if [ "$LANG_MODE" == "zh_CN" ]; then
     print_ok "Installing ibus-rime..."
-    apt install ibus-rime
+    apt install ibus-rime -y
     judge "Install ibus-rime"
 
     print_ok "Setting up ibus..."
@@ -10,10 +10,9 @@ if [ "$LANG_MODE" == "zh_CN" ]; then
     print_ok "Installing Rime schema..."
     zip=https://gitlab.aiursoft.cn/anduin/anduinos-rime/-/archive/master/anduinos-rime-master.zip
     wget $zip -O anduinos-rime.zip && unzip anduinos-rime.zip && rm anduinos-rime.zip
-    rsync -Aavx --update --delete ./anduinos-rime-master/assets/ ~/.config/ibus/rime/
+    mkdir /etc/skel/.config/ibus/rime
+    rsync -Aavx --update --delete ./anduinos-rime-master/assets/ /etc/skel/.config/ibus/rime/
     rm -rf anduinos-rime-master
-    ibus restart
-    ibus engine rime
     judge "Install Rime schema"
 else
     print_warn "Skipping input method installation for $LANG_MODE"
