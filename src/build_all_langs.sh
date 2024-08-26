49d82fe465f7d043ff0f0716dac530ab9c666f0d
Anduin Xue <anduin@aiursoft.com>
2024-08-26 10:13:52 -0400

    chore: Add script to build all language modes
    This commit adds a new script, build_all_langs.sh, which allows for building the application in multiple language modes. The script iterates over an array of language modes and language pack codes, updating the args.sh file with the corresponding values and running the build.sh script. This enhancement improves the localization support and allows for easier testing and deployment in different languages.

---
+++ src/build_all_langs.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+LANG_MODES=("en_US" "zh_CN" "zh_TW" "zh_HK" "ja_JP" "ko_KR" "de_DE" "fr_FR" "es_ES" "ru_RU" "it_IT" "pt_PT" "vi_VN" "th_TH" "ar_SA" "nl_NL" "sv_SE" "pl_PL" "tr_TR")
+LANG_PACK_CODES=("en" "zh" "ja" "ko" "de" "fr" "es" "ru" "it" "pt" "vi" "th" "ar" "nl" "sv" "pl" "tr")
+
+if [[ ! -f "args.sh" || ! -f "build.sh" ]]; then
+  echo "Error: args.sh or build.sh not found."
+  exit 1
+fi
+
+for i in "${!LANG_MODES[@]}"; do
+  sed -i "s/^export LANG_MODE=\".*\"/export LANG_MODE=\"${LANG_MODES[$i]}\"/" args.sh
+  sed -i "s/^export LANG_PACK_CODE=\".*\"/export LANG_PACK_CODE=\"${LANG_PACK_CODES[$i]}\"/" args.sh
+  echo "Running build.sh for LANG_MODE=${LANG_MODES[$i]} and LANG_PACK_CODE=${LANG_PACK_CODES[$i]}"
+  ./build.sh
+done
