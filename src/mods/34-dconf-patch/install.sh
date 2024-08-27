6c3bf964ed71d76aef0fafdb5bbdea411ef40bb1
Anduin Xue <anduin@aiursoft.com>
2024-08-27 14:01:50 +0300

    chore: Update gaming.html for AnduinOS gaming slide
    This commit updates the gaming.html file in the 20-ubiquity-patch/slides directory. The changes include fixing a typo in the URL of the Proton project and removing an extra ">" character. These modifications ensure the accuracy of the information and improve the readability of the gaming slide in AnduinOS.

--- src/mods/34-dconf-patch/install.sh
+++ src/mods/34-dconf-patch/install.sh
@@ -20,28 +20,28 @@ if [ "$LANG_MODE" == "en_US" ]; then
     print_ok "Skipping input method dconf patch for en_US"
 elif [ "$LANG_MODE" == "zh_CN" ]; then
     print_ok "Installing on zh_CN mode, patching dconf for ibus-rime..."
-    dconf write /org/gnome/desktop/input-sources/mru-sources "[('xkb', 'us'), ('ibus', 'rime')]"
-    dconf write /org/gnome/desktop/input-sources/sources "[('xkb', 'us'), ('ibus', 'rime')]"
+    dconf write /org/gnome/desktop/input-sources/mru-sources "[('ibus', 'rime')]"
+    dconf write /org/gnome/desktop/input-sources/sources "[('ibus', 'rime')]"
     judge "Patch dconf for ibus-rime"
 elif [ "$LANG_MODE" == "zh_TW" ]; then
     print_ok "Installing on zh_TW mode, patching dconf for ibus-cangjie and ibus-libzhuyin..."
-    dconf write /org/gnome/desktop/input-sources/mru-sources "[('xkb', 'us'), ('ibus', 'table:cangjie-big'), ('ibus', 'libzhuyin')]"
-    dconf write /org/gnome/desktop/input-sources/sources "[('xkb', 'us'), ('ibus', 'table:cangjie-big'), ('ibus', 'libzhuyin')]"
+    dconf write /org/gnome/desktop/input-sources/mru-sources "[('ibus', 'table:cangjie-big'), ('ibus', 'libzhuyin')]"
+    dconf write /org/gnome/desktop/input-sources/sources "[('ibus', 'table:cangjie-big'), ('ibus', 'libzhuyin')]"
     judge "Patch dconf for ibus-cangjie and ibus-libzhuyin"
 elif [ "$LANG_MODE" == "zh_HK" ]; then
     print_ok "Installing on zh_HK mode, patching dconf for ibus-cangjie..."
-    dconf write /org/gnome/desktop/input-sources/mru-sources "[('xkb', 'us'), ('ibus', 'table:cangjie-big')]"
-    dconf write /org/gnome/desktop/input-sources/sources "[('xkb', 'us'), ('ibus', 'table:cangjie-big')]"
+    dconf write /org/gnome/desktop/input-sources/mru-sources "[('ibus', 'table:cangjie-big')]"
+    dconf write /org/gnome/desktop/input-sources/sources "[('ibus', 'table:cangjie-big')]"
     judge "Patch dconf for ibus-cangjie"
 elif [ "$LANG_MODE" == "ja_JP" ]; then
     print_ok "Installing on ja_JP mode, patching dconf for ibus-anthy..."
-    dconf write /org/gnome/desktop/input-sources/mru-sources "[('xkb', 'us'), ('ibus', 'anthy')]"
-    dconf write /org/gnome/desktop/input-sources/sources "[('xkb', 'us'), ('ibus', 'anthy')]"
+    dconf write /org/gnome/desktop/input-sources/mru-sources "[('ibus', 'anthy')]"
+    dconf write /org/gnome/desktop/input-sources/sources "[('ibus', 'anthy')]"
     judge "Patch dconf for ibus-anthy"
 elif [ "$LANG_MODE" == "ko_KR" ]; then
     print_ok "Installing on ko_KR mode, patching dconf for ibus-hangul..."
-    dconf write /org/gnome/desktop/input-sources/mru-sources "[('xkb', 'us'), ('ibus', 'hangul')]"
-    dconf write /org/gnome/desktop/input-sources/sources "[('xkb', 'us'), ('ibus', 'hangul')]"
+    dconf write /org/gnome/desktop/input-sources/mru-sources "[('ibus', 'hangul')]"
+    dconf write /org/gnome/desktop/input-sources/sources "[('ibus', 'hangul')]"
     judge "Patch dconf for ibus-hangul"
 elif [ "$LANG_MODE" == "de_DE" ]; then
     print_ok "Skipping input method dconf patch for de_DE"
@@ -51,26 +51,26 @@ elif [ "$LANG_MODE" == "es_ES" ]; then
     print_ok "Skipping input method dconf patch for es_ES"
 elif [ "$LANG_MODE" == "ru_RU" ]; then
     print_ok "Installing on ru_RU mode, patching dconf for ibus-m17n..."
-    dconf write /org/gnome/desktop/input-sources/mru-sources "[('xkb', 'us'), ('ibus', 'm17n:ru:kbd')]"
-    dconf write /org/gnome/desktop/input-sources/sources "[('xkb', 'us'), ('ibus', 'm17n:ru:kbd')]"
+    dconf write /org/gnome/desktop/input-sources/mru-sources "[('ibus', 'm17n:ru:kbd')]"
+    dconf write /org/gnome/desktop/input-sources/sources "[('ibus', 'm17n:ru:kbd')]"
 elif [ "$LANG_MODE" == "it_IT" ]; then
     print_ok "Skipping input method dconf patch for it_IT"
 elif [ "$LANG_MODE" == "pt_PT" ]; then
     print_ok "Skipping input method dconf patch for pt_PT"
 elif [ "$LANG_MODE" == "vi_VN" ]; then
     print_ok "Installing on vi_VN mode, patching dconf for ibus-unikey..."
-    dconf write /org/gnome/desktop/input-sources/mru-sources "[('xkb', 'us'), ('ibus', 'Unikey')]"
-    dconf write /org/gnome/desktop/input-sources/sources "[('xkb', 'us'), ('ibus', 'Unikey')]"
+    dconf write /org/gnome/desktop/input-sources/mru-sources "[('ibus', 'Unikey')]"
+    dconf write /org/gnome/desktop/input-sources/sources "[('ibus', 'Unikey')]"
     judge "Patch dconf for ibus-unikey"
 elif [ "$LANG_MODE" == "th_TH" ]; then
     print_ok "Installing on th_TH mode, patching dconf for ibus-libthai..."
-    dconf write /org/gnome/desktop/input-sources/mru-sources "[('xkb', 'us'), ('ibus', 'libthai')]"
-    dconf write /org/gnome/desktop/input-sources/sources "[('xkb', 'us'), ('ibus', 'libthai')]"
+    dconf write /org/gnome/desktop/input-sources/mru-sources "[('ibus', 'libthai')]"
+    dconf write /org/gnome/desktop/input-sources/sources "[('ibus', 'libthai')]"
     judge "Patch dconf for ibus-libthai"
 elif [ "$LANG_MODE" == "ar_SA" ]; then
     print_ok "Installing on ar_SA mode, patching dconf for ibus-m17n..."
-    dconf write /org/gnome/desktop/input-sources/mru-sources "[('xkb', 'us'), ('ibus', 'm17n:ar:kbd')]"
-    dconf write /org/gnome/desktop/input-sources/sources "[('xkb', 'us'), ('ibus', 'm17n:ar:kbd')]"
+    dconf write /org/gnome/desktop/input-sources/mru-sources "[('ibus', 'm17n:ar:kbd')]"
+    dconf write /org/gnome/desktop/input-sources/sources "[('ibus', 'm17n:ar:kbd')]"
 elif [ "$LANG_MODE" == "nl_NL" ]; then
     print_ok "Skipping input method dconf patch for nl_NL"
 elif [ "$LANG_MODE" == "sv_SE" ]; then
