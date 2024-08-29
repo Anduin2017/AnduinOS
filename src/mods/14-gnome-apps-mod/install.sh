9fbb227b3079141b23cb71ff9a33ce4f5fd6af73
Anduin Xue <anduin@aiursoft.com>
2024-08-29 12:35:27 +0000

    Add gnome calendar, todo, characters and font-viewer.

--- src/mods/14-gnome-apps-mod/install.sh
+++ src/mods/14-gnome-apps-mod/install.sh
@@ -10,9 +10,9 @@ apt install -y \
     plymouth plymouth-label plymouth-theme-spinner plymouth-theme-ubuntu-text plymouth-theme-ubuntu-logo \
     gnome-shell gir1.2-gmenu-3.0 gnome-menus gnome-shell-extensions \
     nautilus usb-creator-gtk cheese baobab file-roller gnome-sushi ffmpegthumbnailer \
-    gnome-calculator gnome-disk-utility gnome-control-center gnome-logs \
+    gnome-calculator gnome-disk-utility gnome-control-center gnome-logs gnome-calendar gnome-todo \
     gnome-shell-extension-prefs gnome-shell-extension-desktop-icons-ng gnome-shell-extension-appindicator \
-    gnome-screenshot gnome-system-monitor gnome-sound-recorder \
+    gnome-screenshot gnome-system-monitor gnome-sound-recorder gnome-characters gnome-font-viewer \
     fonts-noto-cjk fonts-noto-core fonts-noto-mono fonts-noto-color-emoji \
     cups system-config-printer cups-bsd \
     ubuntu-drivers-common alsa-utils \
