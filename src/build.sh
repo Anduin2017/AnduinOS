f05b770958fc821975fee3687b7caeece83b5cac
Anduin Xue <anduin@aiursoft.com>
2024-09-03 14:04:42 +0000

    Revert "chore: Update build.sh to create EFI boot image on /isolinux/efiboot.img"
    This reverts commit daac6c6279c49dd8122863a000ea7bc528a456a0.

--- src/build.sh
+++ src/build.sh
@@ -254,15 +254,13 @@ EOF
     print_ok "Creating EFI boot image on /isolinux/efiboot.img..."
     (
         cd isolinux && \
-        sudo dd if=/dev/zero of=efiboot.img bs=1M count=10 && \
-        LOOP_DEVICE=$(sudo losetup --show -f -P efiboot.img) && \
-        sudo mkfs.fat -n 'AnduinOS' -F 32 $LOOP_DEVICE && \
-        sudo mkdir -p efi && \
-        sudo mount $LOOP_DEVICE efi && \
-        sudo grub-install --target=x86_64-efi --efi-directory=efi --uefi-secure-boot --removable --no-nvram $LOOP_DEVICE && \
+        dd if=/dev/zero of=efiboot.img bs=1M count=10 && \
+        sudo mkfs.vfat efiboot.img && \
+        mkdir efi && \
+        sudo mount efiboot.img efi && \
+        sudo grub-install --efi-directory=efi --uefi-secure-boot --removable --no-nvram && \
         sudo umount efi && \
-        sudo losetup --detach $LOOP_DEVICE && \
-        sudo rm -rf efi
+        rm -rf efi
     )
     judge "Create EFI boot image"
 
