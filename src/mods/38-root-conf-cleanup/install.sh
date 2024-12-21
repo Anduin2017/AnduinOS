58135bdaf7d367363bdf352f4af57aab467b49d5
Anduin Xue <anduin@aiursoft.com>
2024-12-21 06:46:35 +0000

    Update installation script to use pipx for uninstalling gnome-extensions-cli

--- src/mods/38-root-conf-cleanup/install.sh
+++ src/mods/38-root-conf-cleanup/install.sh
@@ -3,7 +3,7 @@ set -o pipefail         # exit on pipeline error
 set -u                  # treat unset variable as error
 
 print_ok "Cleaning up /root/.config/ and root's gnome-shell extensions"
-/usr/bin/pip3 uninstall gnome-extensions-cli -y
+/usr/bin/pipx uninstall gnome-extensions-cli
 rm /root/.config/mimeapps.list
 rm /root/.config/dconf -rf
 rm /root/.local/share/gnome-shell/extensions -rf
