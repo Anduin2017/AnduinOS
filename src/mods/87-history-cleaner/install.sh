50ed1f3e4b7d14f601683cd20a8ffe852fa1b772
Anduin Xue <anduin@aiursoft.com>
2024-12-21 17:13:51 +0000

    Add TODO comment regarding potential duplicate sources after apt update

--- src/mods/87-history-cleaner/install.sh
+++ src/mods/87-history-cleaner/install.sh
@@ -8,3 +8,5 @@ judge "Remove bash history and temporary files"
 
 # Disable history in bash to avoid saving commands
 export HISTSIZE=0
+
+# TODO: after build, if run apt update, will get error that duplicate sources
