f4f48b71ffd329dc99023b7d22ca482798443d94
Anduin Xue <anduin@aiursoft.com>
2024-12-21 07:16:51 +0000

    Add TODO comments for unsupported features and update branding ID in installation scripts

--- src/mods/33-gnome-extensions-enabler/install.sh
+++ src/mods/33-gnome-extensions-enabler/install.sh
@@ -4,15 +4,15 @@ set -u                  # treat unset variable as error
 
 print_ok "Enabling gnome extensions for root..."
 /root/.local/bin/gext -F enable arcmenu@arcmenu.com
-/root/.local/bin/gext -F enable sound-output-device-chooser@kgshank.net
-/root/.local/bin/gext -F enable proxyswitcher@flannaghan.com
-/root/.local/bin/gext -F enable blur-my-shell@aunetx
+/root/.local/bin/gext -F enable sound-output-device-chooser@kgshank.net # TODO: Not supported.
+/root/.local/bin/gext -F enable proxyswitcher@flannaghan.com # TODO: Not supported.
+/root/.local/bin/gext -F enable blur-my-shell@aunetx # TODO: Not supported.
 /root/.local/bin/gext -F enable customize-ibus@hollowman.ml
 /root/.local/bin/gext -F enable dash-to-panel@jderose9.github.com
 /root/.local/bin/gext -F enable network-stats@gnome.noroadsleft.xyz
-/root/.local/bin/gext -F enable openweather-extension@jenslody.de
-/root/.local/bin/gext -F enable switcher@anduinos
-/root/.local/bin/gext -F enable rounded-window-corners@yilozt
+/root/.local/bin/gext -F enable openweather-extension@jenslody.de # TODO: Not supported.
+/root/.local/bin/gext -F enable switcher@anduinos # TODO: Not supported.
+/root/.local/bin/gext -F enable rounded-window-corners@yilozt # TODO: Not supported.
 /root/.local/bin/gext -F enable lockkeys@vaina.lt
 /root/.local/bin/gext -F enable tiling-assistant@leleat-on-github
 judge "Enable gnome extensions"
--- src/mods/34-dconf-patch/install.sh
+++ src/mods/34-dconf-patch/install.sh
@@ -10,6 +10,9 @@ set -u                  # treat unset variable as error
 print_ok "Loading dconf settings"
 export $(dbus-launch)
 
+# TODO: Overview was shown. Not working properly
+# Power button was shown, but not working properly
+
 dconf load /org/gnome/ < ./dconf.ini
 dconf write /org/gtk/settings/file-chooser/sort-directories-first true
 dconf write /org/gnome/desktop/input-sources/xkb-options "@as []"
