b367ad221e9c784a788e06be7699704fe00f64e0
Anduin Xue <anduin@aiursoft.com>
2024-12-23 06:51:26 +0000

    Enable ProxySwitcher GNOME extension in installation script

--- src/mods/33-gnome-extensions-enabler/install.sh
+++ src/mods/33-gnome-extensions-enabler/install.sh
@@ -5,7 +5,7 @@ set -u                  # treat unset variable as error
 print_ok "Enabling gnome extensions for root..."
 /root/.local/bin/gext -F enable arcmenu@arcmenu.com
 /root/.local/bin/gext -F enable sound-output-device-chooser@kgshank.net # TODO: Not supported.
-/root/.local/bin/gext -F enable ProxySwitcher@flannaghan.com # TODO: Not supported.
+/root/.local/bin/gext -F enable ProxySwitcher@flannaghan.com
 /root/.local/bin/gext -F enable customize-ibus@hollowman.ml
 /root/.local/bin/gext -F enable dash-to-panel@jderose9.github.com
 /root/.local/bin/gext -F enable network-stats@gnome.noroadsleft.xyz
