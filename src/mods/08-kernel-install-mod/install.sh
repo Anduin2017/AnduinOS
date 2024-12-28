348b1e0562d7aa37a77a965d4be0be22699e14d0
Anduin Xue <anduin@aiursoft.com>
2024-12-28 07:27:42 +0000

    Reorder installation steps for clarity and add live-boot installation

--- src/mods/08-kernel-install-mod/install.sh
+++ src/mods/08-kernel-install-mod/install.sh
@@ -2,13 +2,15 @@ set -e                  # exit on error
 set -o pipefail         # exit on pipeline error
 set -u                  # treat unset variable as error
 
-print_ok "Installing kernel package $TARGET_KERNEL_PACKAGE..."
+print_info "Installing live-boot..."
 waitNetwork
 apt install -y \
     casper \
     discover \
     laptop-detect \
-    os-prober \
+    os-prober
+judge "Install live-boot"
 
+print_ok "Installing kernel package $TARGET_KERNEL_PACKAGE..."
 apt install -y --no-install-recommends $TARGET_KERNEL_PACKAGE
 judge "Install kernel package"
