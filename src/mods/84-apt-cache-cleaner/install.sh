fbc0798024146145c5348fbb4b3ee41c3876f814
Anduin Xue <anduin@aiursoft.com>
2024-12-30 14:59:34 +0000

    Update install script to include apt update before cleaning cache

--- src/mods/84-apt-cache-cleaner/install.sh
+++ src/mods/84-apt-cache-cleaner/install.sh
@@ -3,8 +3,8 @@ set -o pipefail         # exit on pipeline error
 set -u                  # treat unset variable as error
 
 print_ok "Cleaning up apt cache..."
+apt update
 apt clean -y
 rm -rf /var/cache/apt/archives/*
-rm -rf /var/lib/apt/lists/*
 rm -v /var/cache/debconf/*.dat-old
 judge "Clean up apt cache"
