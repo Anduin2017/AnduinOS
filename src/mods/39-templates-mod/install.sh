cc8bc215c92090093cf70c649e8fd7cb5ce9cac2
Florin Tanasa <florin.tanasa@svgenebank.ro>
2025-07-24 13:17:59 +0300

    Adds templates configuration
    Configures templates for different languages.
    
    Introduces language-specific template directories and default Markdown files within /etc/skel.

--- src/mods/39-templates-mod/install.sh
+++ src/mods/39-templates-mod/install.sh
@@ -2,11 +2,35 @@ set -e                  # exit on error
 set -o pipefail         # exit on pipeline error
 set -u                  # treat unset variable as error
 
-print_ok "Configuring templates..."
-mkdir -p /etc/skel/Templates
-touch /etc/skel/Templates/Text.txt
-touch /etc/skel/Templates/Markdown.md
-cat << 'EOF' > /etc/skel/Templates/Markdown.md
+print_ok "Declare Templates dir name as variable for specific languages..."
+case $LANG_MODE in
+    "ro_RO")
+        export TEMPLATE_DIR="Șabloane"
+        print_ok "Configuring templates..."
+        mkdir -p /etc/skel/$TEMPLATE_DIR
+        touch /etc/skel/$TEMPLATE_DIR/Text.txt
+        touch /etc/skel/$TEMPLATE_DIR/Markdown.md
+cat << 'EOF' > /etc/skel/$TEMPLATE_DIR/Markdown.md
+# Titlu
+
+- [ ] De realizat 1
+- [ ] De realizat 2
+- [ ] De realizat 3
+
+## Subtitlu
+
+1. Lista 1
+2. Lista 2
+3. Lista 3
+EOF
+        ;;
+    *)
+        export TEMPLATE_DIR="Templates"
+        print_ok "Configuring templates..."
+        mkdir -p /etc/skel/$TEMPLATE_DIR
+        touch /etc/skel/$TEMPLATE_DIR/Text.txt
+        touch /etc/skel/$TEMPLATE_DIR/Markdown.md
+cat << 'EOF' > /etc/skel/$TEMPLATE_DIR/Markdown.md
 # Title
 
 - [ ] Task 1
@@ -19,4 +43,6 @@ cat << 'EOF' > /etc/skel/Templates/Markdown.md
 2. Numbered 2
 3. Numbered 3
 EOF
+        ;;
+esac
 judge "Configure templates"
