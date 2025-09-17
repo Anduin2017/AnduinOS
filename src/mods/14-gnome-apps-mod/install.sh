a5ded28c8d404fe9d80215d3c4f24f3dfca39856
Anduin Xue <anduin@aiursoft.com>
2025-09-17 03:25:36 +0000

    Add printer-driver-all to gnome fonts installation

--- src/mods/14-gnome-apps-mod/install.sh
+++ src/mods/14-gnome-apps-mod/install.sh
@@ -164,7 +164,7 @@ judge "Install ibus"
 
 print_ok "Installing gnome fonts..."
 apt install $INTERACTIVE \
-    fonts-noto-cjk fonts-noto-core fonts-noto-mono fonts-noto-color-emoji --no-install-recommends
+    fonts-noto-cjk fonts-noto-core fonts-noto-mono fonts-noto-color-emoji printer-driver-all --no-install-recommends
 judge "Install gnome fonts"
 
 print_ok "Installing gnome printer support..."
