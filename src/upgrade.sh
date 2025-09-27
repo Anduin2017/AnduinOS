6c5a17a4c6bf0027c4691da52dd17169156cc654
Anduin <anduin@aiursoft.com>
2025-09-27 11:16:50 +0000

    Patch Arc Menu logo, text, and localization for improved user experience

--- src/upgrade.sh
+++ src/upgrade.sh
@@ -461,6 +461,31 @@ EOF
     # dconf write  /org/gnome/shell/extensions/arcmenu/arcmenu-hotkey "['Super_L', 'Super_R']"
     # judge "Add hotkey for ArcMenu"
 
+    print_ok "Patch Arc Menu logo..."
+    wget -O ./logo.svg https://gitlab.aiursoft.cn/anduin/anduinos/-/raw/1.4/src/mods/30-gnome-extension-arcmenu-patch/logo.svg?ref_type=heads
+    sudo mv ./logo.svg /usr/share/gnome-shell/extensions/arcmenu@arcmenu.com/icons/anduinos-logo.svg
+    judge "Patch Arc Menu logo"
+
+    print_ok "Patch Arc Menu text..."
+    sudo sed -i 's/Unpin from ArcMenu/Unpin from Start menu/g' /usr/share/gnome-shell/extensions/arcmenu@arcmenu.com/appMenu.js
+    sudo sed -i 's/Pin to ArcMenu/Pin to Start menu/g' /usr/share/gnome-shell/extensions/arcmenu@arcmenu.com/appMenu.js
+    judge "Patch Arc Menu text"
+
+    print_ok "Patch Arc Menu localization..."
+    sudo msgunfmt /usr/share/gnome-shell/extensions/arcmenu@arcmenu.com/locale/zh_CN/LC_MESSAGES/arcmenu.mo -o /tmp/arcmenu.po
+    cat << EOF | sudo tee -a /tmp/arcmenu.po > /dev/null
+msgid "Pin to Start menu"
+msgstr "固定到开始菜单"
+
+msgid "Unpin from Start menu"
+msgstr "从开始菜单取消固定"
+EOF
+
+    sudo sed -i "s/新建/新增/g" /tmp/arcmenu.po # Only zh_CN need this fix.
+    msgfmt /tmp/arcmenu.po -o /usr/share/gnome-shell/extensions/arcmenu@arcmenu.com/locale/zh_CN/LC_MESSAGES/arcmenu.mo
+    judge "Patch Arc Menu localization"
+
+
     SERVICE_FILE="/etc/systemd/user/deskmon.service"
     if [ ! -f "${SERVICE_FILE}" ]; then
         print_error "Deskmon service file not found at ${SERVICE_FILE}. Please ensure deskmon is installed."
