b203a1b22ec776c7c356c5012f4913523c539c5d
Anduin Xue <anduin@aiursoft.com>
2025-09-29 06:14:48 +0000

    Release 1.1.10

--- src/upgrade.sh
+++ src/upgrade.sh
@@ -6,7 +6,7 @@ set -e                  # exit on error
 set -o pipefail         # exit on pipeline error
 set -u                  # treat unset variable as error
 export DEBIAN_FRONTEND=noninteractive
-export LATEST_VERSION="1.1.9"
+export LATEST_VERSION="1.1.10"
 export CODE_NAME="noble"
 export OS_ID="AnduinOS"
 export CURRENT_VERSION=$(cat /etc/lsb-release | grep DISTRIB_RELEASE | cut -d "=" -f 2)
@@ -578,6 +578,7 @@ function main() {
               upgrade_116_to_117
               upgrade_117_to_118
               upgrade_118_to_119
+              upgrade_119_to_1110
               ;;
           "1.1.1")
               upgrade_111_to_112
@@ -588,6 +589,7 @@ function main() {
               upgrade_116_to_117
               upgrade_117_to_118
               upgrade_118_to_119
+              upgrade_119_to_1110
               ;;
           "1.1.2")
               upgrade_112_to_113
@@ -597,6 +599,7 @@ function main() {
               upgrade_116_to_117
               upgrade_117_to_118
               upgrade_118_to_119
+              upgrade_119_to_1110
               ;;
           "1.1.3")
               upgrade_113_to_114
@@ -605,6 +608,7 @@ function main() {
               upgrade_116_to_117
               upgrade_117_to_118
               upgrade_118_to_119
+              upgrade_119_to_1110
               ;;
           "1.1.4")
               upgrade_114_to_115
@@ -612,26 +616,34 @@ function main() {
               upgrade_116_to_117
               upgrade_117_to_118
               upgrade_118_to_119
+              upgrade_119_to_1110
               ;;
           "1.1.5")
               upgrade_115_to_116
               upgrade_116_to_117
               upgrade_117_to_118
               upgrade_118_to_119
+              upgrade_119_to_1110
               ;;
           "1.1.6")
               upgrade_116_to_117
               upgrade_117_to_118
               upgrade_118_to_119
+              upgrade_119_to_1110
               ;;
           "1.1.7")
               upgrade_117_to_118
               upgrade_118_to_119
+              upgrade_119_to_1110
               ;;
           "1.1.8")
               upgrade_118_to_119
+              upgrade_119_to_1110
               ;;
           "1.1.9")
+              upgrade_119_to_1110
+              ;;
+          "1.1.10")
               print_ok "Your system is already up to date. No update available."
               exit 0
               ;;
