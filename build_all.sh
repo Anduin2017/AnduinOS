3f085ef60c303e37c83fb45bbb567b0cde473db1
Anduin Xue <anduin@aiursoft.com>
2025-10-28 10:45:33 +0000

    Update URLs from gitlab.aiursoft.cn to gitlab.aiursoft.com and git.aiursoft.cn to git.aiursoft.com in scripts

--- build_all.sh
+++ build_all.sh
@@ -148,7 +148,7 @@ shopt -s extglob
     mktorrent "${trackers[@]}" -o "${base}.torrent" "$iso"
   done
 
-  burner="https://gist.aiursoft.cn/anduin/7ef713580cc641e68ebe1f22f4952e7c/raw/HEAD/burn.sh"
+  burner="https://gist.aiursoft.com/anduin/7ef713580cc641e68ebe1f22f4952e7c/raw/HEAD/burn.sh"
   wget -O burn.sh "$burner"
   chmod +x burn.sh
 )
