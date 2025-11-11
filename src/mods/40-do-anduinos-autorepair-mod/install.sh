4b47f1f7f0d5a00f7e4be8fc022d361c42fb7fd2
Anduin Xue <anduin@aiursoft.com>
2025-11-11 15:50:07 +0000

    Fix command name in installation script for do_anduinos_autorepair

--- src/mods/40-do-anduinos-autorepair-mod/install.sh
+++ src/mods/40-do-anduinos-autorepair-mod/install.sh
@@ -3,6 +3,6 @@ set -o pipefail         # exit on pipeline error
 set -u                  # treat unset variable as error
 
 print_ok "Adding new command to this OS: do_anduinos_upgrade..."
-cp ./do-anduinos-autorepair.sh /usr/local/bin/do_anduinos_autorepair
-chmod +x /usr/local/bin/do_anduinos_autorepair
-judge "Add new command do_anduinos_autorepair"
+cp ./do-anduinos-autorepair.sh /usr/local/bin/do-anduinos-autorepair
+chmod +x /usr/local/bin/do-anduinos-autorepair
+judge "Add new command do-anduinos-autorepair"
