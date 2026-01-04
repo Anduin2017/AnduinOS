71ede279a3a3c5f3f777f40b5d328692136929f2
Anduin <anduin@aiursoft.com>
2026-01-04 18:56:08 +0000

    fix: add -y flag to apt-get install for non-interactive installation

--- src/upgrade.sh
+++ src/upgrade.sh
@@ -594,7 +594,7 @@ function upgrade_138_to_139() {
     # gstreamer plugins and tools
     print_ok "Installing GStreamer plugins and tools..."
     sudo apt-get update
-    sudo apt-get install \
+    sudo apt-get install -y \
       gstreamer1.0-plugins-base \
       gstreamer1.0-plugins-good \
       gstreamer1.0-plugins-bad \
