9fc66ee3aedac54ba526b5f175d0eaeb00635836
Anduin Xue <anduin@aiursoft.com>
2026-03-02 16:52:05 +0000

    Edit build.sh

--- src/build.sh
+++ src/build.sh
@@ -58,7 +58,7 @@ function setup_host() {
 
 function download_base_system() {
     print_ok "Calling debootstrap to download base debian system..."
-    sudo debootstrap  --arch=amd64 --variant=minbase --include=git $TARGET_UBUNTU_VERSION new_building_os $BUILD_UBUNTU_MIRROR
+    sudo debootstrap  --arch=amd64 --variant=minbase --include=git,ca-certificates $TARGET_UBUNTU_VERSION new_building_os $BUILD_UBUNTU_MIRROR
     judge "Download base system"
 }
 
